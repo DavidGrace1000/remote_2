ac98134e6f0e0528ba2698d4235bb8ce0ace726f
DavidGrace1000 <putra06pati@gmail.com>
2023-12-22 09:25:20 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset = "UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv= "X-UA-Compatible">
+        <title>Coba Git remote</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
